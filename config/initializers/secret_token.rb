81dbb6f3c355e004417428f8f312707113a72605
RJ Dabbar <rjdabbar@gmail.com>
2014-05-05 11:21:05 -0400

    gems updated for dev

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScorecardTest::Application.config.secret_key_base = 'a57e79e8acfae3cf60f63d4b73706be46dc354a7146f03c052ada30b9fa73d8d8a8bcb58c7032a3b3dc147b8e0ab36035250a0ee3858567435a69e64a5594ac4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ScorecardTest::Application.config.session_store :cookie_store, key: '_scorecard_test_session'
